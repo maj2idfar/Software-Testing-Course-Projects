aa51ac665d21623aba0f048820121ec72ca0f4e2
Ouldouz-Neysari <neysariouldouz@gmail.com>
2023-10-30 21:14:38 +0330

    SignUp controllers added

--- CA1/src/test/java/controllers/AuthenticationControllerTest.java
+++ CA1/src/test/java/controllers/AuthenticationControllerTest.java
@@ -2,6 +2,8 @@ package controllers;
 
 import exceptions.IncorrectPassword;
 import exceptions.NotExistentUser;
+import exceptions.UsernameAlreadyTaken;
+import model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -21,11 +23,13 @@ public class AuthenticationControllerTest {
     @Mock private Baloot baloot;
     @Mock private NotExistentUser notExistentUser;
     @Mock private IncorrectPassword incorrectPassword;
+    @Mock private UsernameAlreadyTaken usernameAlreadyTaken;
     private AuthenticationController authenticationController;
 
     @BeforeEach
     public void setUp() {
         baloot = mock(Baloot.class);
+
         authenticationController = new AuthenticationController();
         authenticationController.setBaloot(baloot);
     }
@@ -82,5 +86,46 @@ public class AuthenticationControllerTest {
         assertEquals(incorrectPassword.getMessage(), response.getBody());
 
         verify(baloot, times(1)).login("user", "pass");
+    }
+
+    @Test
+    public void successfulSignup() throws UsernameAlreadyTaken {
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "user");
+        input.put("password", "pass");
+        input.put("email", "user@test.com");
+        input.put("birthDate", "2000/1/1");
+        input.put("address", "nowhere!");
+
+        doNothing().when(baloot).addUser(isA(User.class));
+
+        ResponseEntity<String> response = authenticationController.signup(input);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("signup successfully!", response.getBody());
+
+        verify(baloot, times(1)).addUser(isA(User.class));
+    }
+
+    @Test
+    public void signupWithExistedUsername() throws UsernameAlreadyTaken {
+        usernameAlreadyTaken = mock(UsernameAlreadyTaken.class);
+
+        Map<String, String> input = new HashMap<>();
+        input.put("username", "user");
+        input.put("password", "pass");
+        input.put("email", "user@test.com");
+        input.put("birthDate", "2000/1/1");
+        input.put("address", "nowhere!");
+
+        doThrow(usernameAlreadyTaken).when(baloot).addUser(isA(User.class));
+        when(usernameAlreadyTaken.getMessage()).thenReturn("Username already taken!");
+
+        ResponseEntity<String> response = authenticationController.signup(input);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(usernameAlreadyTaken.getMessage(), response.getBody());
+
+        verify(baloot, times(1)).addUser(isA(User.class));
     }
 }
